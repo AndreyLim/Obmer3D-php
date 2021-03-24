7707c0c42b0cddb9857b6773b8c47e67c4e7c0d5
Andrew <skazlipop.88@mail.ru@>
2021-03-24 11:15:54 +0300

    begin: 360 adaptive

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ function swa2() {
   b.style.opacity = '0';
 }
 
-const postData = body => fetch('https://andreylimin.ru/send.php', {
+const postData = body => fetch('http://3d.akspro.ru/send.php', {
   method: 'POST',
   headerd: {
     'Content-Type': 'application/json'
